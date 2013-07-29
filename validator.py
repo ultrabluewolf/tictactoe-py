5a737594efdbdf5815a1a384647fb2772327d2df
Britney L <ultrabluewolf-fb2@yahoo.com>
2013-07-29 19:26:08 -0400

    added is_winner functionality

--- validator.py
+++ validator.py
@@ -21,16 +21,67 @@ def is_winner(board):
 		result = check_col_win(board,col)
 		if not is_blank(result):
 			return result
+	
+	return Board.BLANK
 
 def check_row_win(board,row):
-	pass
+	crosses=[]
+	circs=[]
+	for col in range(Board.MAX):
+		if is_cross(board[row][col]):
+			crosses.append(Board.CROSS)
+			if len(crosses) == Board.MAX:
+				return Board.CROSS
+		elif is_circ(board[row][col]):
+			circs.append(Board.CIRC)
+			if len(circs) == Board.MAX:
+				return Board.CIRC
+	return Board.BLANK
 
 def check_col_win(board,col):
-	pass
+	crosses=[]
+	circs=[]
+	for row in range(Board.MAX):
+		if is_cross(board[row][col]):
+			crosses.append(Board.CROSS)
+			if len(crosses) == Board.MAX:
+				return Board.CROSS
+		elif is_circ(board[row][col]):
+			circs.append(Board.CIRC)
+			if len(circs) == Board.MAX:
+				return Board.CIRC
+	return Board.BLANK
 
 def check_diagonal_win(board,dgn):
-	pass
+	crosses=[]
+	circs=[]
+	if dgn == 0:
+		for i in range(Board.MAX):
+			if is_cross(board[i][i]):
+					crosses.append(Board.CROSS)
+					if len(crosses) == Board.MAX:
+						return Board.CROSS
+			elif is_circ(board[i][i]):
+				circs.append(Board.CIRC)
+				if len(circs) == Board.MAX:
+					return Board.CIRC
+	elif dgn == 1:
+		col = Board.MAX-1
+		for row in range(Board.MAX):
+			if is_cross(board[row][col]):
+				crosses.append(Board.CROSS)
+				if len(crosses) == Board.MAX:
+					return Board.CROSS
+			elif is_circ(board[row][col]):
+				circs.append(Board.CIRC)
+				if len(circs) == Board.MAX:
+					return Board.CIRC
+			col-=1
+	return Board.BLANK
 
 def is_blank(sym):
 	return sym == Board.BLANK
-
+def is_cross(sym):
+	return sym == Board.CROSS
+def is_circ(sym):
+	return sym == Board.CIRC
