c145078ba8154e426da44f6a229c2ea8b162ff1d
Britney L <ultrabluewolf-fb2@yahoo.com>
2013-07-29 18:37:40 -0400

    added simple test, fixed read_input, added temp input

--- tests.py
+++ tests.py
@@ -1,7 +1,15 @@
+#!/usr/bin/env python
+
 """..."""
 
 import board,player,validator,utils
 from board import *
+from validator import *
 
 print "some tests..."
+boards = utils.read_input("input")
 
+for i in range(len(boards)):
+	print str(boards[i])
+	print is_winner(boards[i])
+	print "\n"
--- utils.py
+++ utils.py
@@ -1,6 +1,34 @@
-"""..."""
-
-class Utils:
+"""utils"""
+import board
+from board import *
 	
-	def read_input(fname):
-		pass
+def read_input(fname):
+# read in input for checker tests
+	boards = []
+	with open(fname,'r') as file:
+		row=0
+		col=0
+		boards_idx=0
+		boards.append(Board())
+		for line in file:
+			line = line.strip()
+			#print line
+			if len(line) == 0:
+				continue
+			for sym in line:
+				#print sym
+				boards[boards_idx][row][col]=sym.upper()
+				if col < Board.MAX-1:
+					col+=1
+				else:
+					if row < Board.MAX-1:
+						row+=1
+						col=0
+					else:
+						#print "LIM Reached!"
+						boards.append(Board())
+						boards_idx+=1
+						row=0
+						col=0
+	return boards[:len(boards)-1]
+
