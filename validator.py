1abcbc6c69bd24a22a8d84d00f8bb95ef2f4d133
Britney L <ultrabluewolf-fb2@yahoo.com>
2013-07-28 23:06:03 -0400

    started validator code

---
+++ validator.py
@@ -0,0 +1,35 @@
+"""Board Validation methods"""
+
+import board
+from board import *
+
+"""check board for a winner"""
+def is_winner(board):
+	result = Board.BLANK
+	
+	for dgn in range(Board.DGN):
+		result = check_diagonal_win(board,dgn)
+		if not is_blank(result):
+			return result
+	
+	for row in range(Board.MAX):
+		result = check_row_win(board,row)
+		if not is_blank(result):
+			return result
+	
+	for col in range(Board.MAX):
+		result = check_col_win(board,col)
+		if not is_blank(result):
+			return result
+
+def check_row_win(board,row):
+	pass
+
+def check_col_win(board,col):
+	pass
+
+def check_diagonal_win(board,dgn):
+	pass
+
+def is_blank(sym):
+	return sys == Board.BLANK
