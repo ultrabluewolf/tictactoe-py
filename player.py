5939f8e599d4bbb55d6ed309698f333d342c8b1c
Britney L <ultrabluewolf-fb2@yahoo.com>
2013-07-29 20:07:54 -0400

    minor edits to skeleton

--- player.py
+++ player.py
@@ -6,8 +6,11 @@ from board import *
 
 class Player:
 	
-	def __init__(self):
+	def __init__(self,sym,label,is_ai):
 		self.rand = Random()
+		self.sym=sym
+		self.is_ai=is_ai
+		self.label=label
 
 	"""TODO:examine board give next move"""
 	def next_move(self,board):
