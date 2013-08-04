14f1eef3d0a7b096f8de54392dd68f572f2bf5b6
Britney L <ultrabluewolf-fb2@yahoo.com>
2013-08-03 22:14:55 -0400

    added cmdline flags to signal cli mode

--- driver.py
+++ driver.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
 """..."""
-import engine,player
+import engine,player,argparse
 from engine import *
 from player import *
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-c','--cli',action='store_false',help='commandline interface mode')
+args = parser.parse_args()
+
 game=Engine()
-game.run(True)
+game.run(args.cli)
